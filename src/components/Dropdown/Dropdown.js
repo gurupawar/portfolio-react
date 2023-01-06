0b20fda3041dea2618be3b72535b59ccd26a7b2b
Soufiane sejjari <81421925+soufianesejjari@users.noreply.github.com>
2023-01-06 20:42:24 +0100

    Update Dropdown.js

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -89,7 +89,7 @@ function Dropdown({ isOpen, toggle }) {
       <NavBtn onClick={toggle}>
         <a
           className="btn PrimaryBtn"
-          href="https://github.com/gurupawar/"
+          href="https://github.com/soufianesejjari/"
           target="_blank"
           rel="noopener noreferrer"
         >
